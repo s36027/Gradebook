e48820474159df2981221d5b2fbe6f8a0ea8a29a
s36027 <s36027@pjwstk.edu.pl>
2026-04-17 20:10:01 +0200

    zadanie 1 update 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Gradebook gb = new Gradebook();
-        gb.addSubject("Math");
-        gb.addGrade("Math", 5.0);
-
-        System.out.println("Subjects: " + gb.getSubjects());
-        System.out.println("Average: " + gb.calcAvgForSubject("Math"));
+        System.out.println("Sukces");
     }
 }
